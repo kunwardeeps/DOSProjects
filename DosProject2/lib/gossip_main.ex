d22864d196266370a5aef3a29a67db7db5aa873d
gayatrib12 <gayatri.behera@ufl.edu>
2018-09-29 16:12:16 -0400

    3d topology implementation

--- DosProject2/lib/gossip_main.ex
+++ DosProject2/lib/gossip_main.ex
@@ -34,9 +34,50 @@ defmodule GossipMain do
   def init_nodes(numNodes, i, topology) do
     if (i <= numNodes) do
       {:ok, pid} = GenServer.start_link(Gossip.Node, [i, numNodes, 0, topology, self()])
-      GossipPushSum.Registry.put(i, pid)
+
+      IO.puts(topology)
+
+      register_process(i, topology, numNodes, pid)
+
       Process.monitor(pid)
       init_nodes(numNodes, i+1, topology)
     end
   end
+
+  def register_process(i, topology, numNodes, pid) do
+    case topology do
+      "full_network" -> GossipPushSum.Registry.put(i, [i,0,0,pid])
+      "line" -> GossipPushSum.Registry.put(i, [i,0,0,pid])
+      #"imperfect_line" -> register_process_imperfect_line
+      "random_2d" -> GossipPushSum.Registry.put(i, [:rand.uniform(numNodes)/numNodes, :rand.uniform(numNodes)/numNodes, 0, pid])
+      #"3d" -> assign3d_grid(i, numNodes)
+
+      register_process_3d(i, numNodes)
+    end
+
+
+    GossipPushSum.Registry.put(i, pid)
+  end
+
+  def register_process_3d(i, numNodes) do
+
+    cube_root = numNodes |> :math.pow(1/3) |> :math.ceil |> :erlang.trunc
+    cube_root_sq = cube_root |> :math.pow(2) |> :erlang.trunc
+    z = i/cube_root_sq |> :math.ceil|> :erlang.trunc
+    rem_z = i |> rem(cube_root_sq)
+    if (rem_z == 0) do
+      [cube_root, cube_root, z]
+    else
+      y = rem_z / cube_root |> :math.ceil |> :erlang.trunc
+      rem_y = rem_z |> rem(cube_root)
+      if rem_y == 0 do
+        [cube_root, y, z]
+      else
+        x = rem_y
+        [x, y, z]
+      end
+    end
+
+  end
+
 end
